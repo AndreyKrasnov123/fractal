14f63a3d9d3c547229c4a2847977a31c6d4db439
Andrey_____ <151596221+AndreyKrasnov123@users.noreply.github.com>
2024-05-04 11:56:36 +0700

    Add files via upload

---
+++ fractal-main/src/main.py
@@ -0,0 +1,13 @@
+import subprocess
+
+libraries = ['numpy', 'matplotlib', 'customtkinter']
+for lib in libraries:
+    subprocess.call(['pip', 'install', lib])
+
+
+from interface import CustomFractalApp
+
+
+if __name__ == "__main__":
+    app_instance = CustomFractalApp()
+    app_instance.run_app()
